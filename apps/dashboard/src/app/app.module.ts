6d29408ae1573d3e726552f1c721c9411c3bd7a0
owezzy <owenobezzy@gmail.com>
2019-02-12 19:10:13 +0300

    import routing and login modules

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -9,18 +9,18 @@ import { MaterialModule } from '@workshop/material';
 import { HomeModule } from './home/home.module';
 import { ProjectsModule } from './projects/projects.module';
 import { CustomersModule } from './customers/customers.module';
-
+import { AppRoutingModule } from './app-routing.module';
+import { UiLoginModule } from "@workshop/ui-login";
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     NxModule.forRoot(),
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
     BrowserAnimationsModule,
     MaterialModule,
-    HomeModule,
-    ProjectsModule,
-    CustomersModule
+    UiLoginModule,
+    AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
