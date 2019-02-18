b4dd0e89d4da33570a1d0e50104773a9caf30a3c
owezzy <owenobezzy@gmail.com>
2019-02-18 16:24:34 +0300

    create projects object to load on ngOninit, with get and delete projects methods

--- apps/dashboard/src/app/projects/projects.component.ts
+++ apps/dashboard/src/app/projects/projects.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Project, ProjectsService } from '@workshop/core-data';
+
 
 @Component({
   selector: 'app-projects',
@@ -6,39 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
-  projects = [
-    {
-      id: '1',
-      title: 'Project One',
-      details: 'This is a sample project',
-      percentComplete: 20,
-      approved: false
-    },
-    {
-      id: '2',
-      title: 'Project Two',
-      details: 'This is a sample project',
-      percentComplete: 40,
-      approved: false
-    },
-    {
-      id: '3',
-      title: 'Project Three',
-      details: 'This is a sample project',
-      percentComplete: 100,
-      approved: true
-    }
-  ];
+  projects$;
+  selectedProject: Project;
 
-  selectedProject;
+  constructor(private projectService: ProjectsService) {}
 
-  constructor() {}
-
-  ngOnInit() {}
+  ngOnInit() {
+    this.getProjects()
+  }
 
   selectProject(project){
-    this.selectedProject = project
+    this.selectedProject = project;
     console.log('SELECTED PROJECT', project);
+  }
+
+  getProjects(){
+   this.projects$ = this.projectService.all()
+  }
+
+  deleteProject(project){
+    this.projectService.delete(project.id)
+      .subscribe(result => this.getProjects())
   }
 
   cancel() {
