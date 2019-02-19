912074cf304ac11428872ce51e62555e84f40b38
owezzy <owenobezzy@gmail.com>
2019-02-19 13:20:36 +0300

    remove extra route

--- apps/dashboard/src/app/app-routing.module.ts
+++ apps/dashboard/src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from '../../../../libs/ui-login/src/lib/login/login.component';
 
 const routes: Routes = [
   { path: '', loadChildren: './home/home.module#HomeModule' },
   { path: 'projects', loadChildren: './projects/projects.module#ProjectsModule' },
   { path: 'customers', loadChildren: './customers/customers.module#CustomersModule' },
-  { path: 'login', component: LoginComponent  },
 ];
 
 @NgModule({
