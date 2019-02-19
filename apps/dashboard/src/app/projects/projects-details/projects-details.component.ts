4d78b6512d0dec3b986d8b7a78fd232d6fc7c5a1
owezzy <owenobezzy@gmail.com>
2019-02-19 16:28:07 +0300

    generate projects detail component

---
+++ apps/dashboard/src/app/projects/projects-details/projects-details.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Project } from '@workshop/core-data';
+
+@Component({
+  selector: 'app-projects-details',
+  templateUrl: './projects-details.component.html',
+  styleUrls: ['./projects-details.component.css']
+})
+export class ProjectsDetailsComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
