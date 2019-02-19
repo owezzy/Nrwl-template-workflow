1ac6e2239d072bff5378a531fb0e87d68779e271
owezzy <owenobezzy@gmail.com>
2019-02-19 17:14:37 +0300

    project detail presentation component and view

--- apps/dashboard/src/app/projects/projects-details/projects-details.component.ts
+++ apps/dashboard/src/app/projects/projects-details/projects-details.component.ts
@@ -1,17 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Project } from '@workshop/core-data';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-projects-details',
   templateUrl: './projects-details.component.html',
   styleUrls: ['./projects-details.component.css']
 })
-export class ProjectsDetailsComponent implements OnInit {
+export class ProjectsDetailsComponent {
+  @Input() project;
+  @Output() saved = new EventEmitter();
+  @Output() cancelled = new EventEmitter();
 
-  constructor() {
-  }
 
-  ngOnInit() {
-  }
 
 }
